34a07e950a54762197067c1c977f1f19050f3b52
Yohan Vinu <yohanvvinu@gmail.com>
2023-01-29 18:49:38 +0530

    Implemented the edit button and enabled dynamic listbox

--- gui.py
+++ gui.py
@@ -4,9 +4,12 @@ import PySimpleGUI as sg
 label = sg.Text("Type in a to-do: ")
 input_box = sg.InputText(tooltip="Enter to-do", key="todo")
 add_button = sg.Button("Add")
+list_box = sg.Listbox(values=functions.get_todos(), key="todos",
+                      enable_events=True, size=[40, 10])
+edit_button = sg.Button("Edit")
 
 window = sg.Window("To-do Desktop",
-                   layout=[[label], [input_box, add_button]],
+                   layout=[[label], [input_box, add_button], [list_box, edit_button]],
                    font=('Helvetica', 20))
 
 todos = functions.get_todos()
@@ -14,9 +17,23 @@ todos = functions.get_todos()
 while True:
     event, values = window.read()
     match event:
+
         case "Add":
             todos.append(values["todo"] + "\n")
             functions.write_todos(todos)
+            window['todos'].update(values=todos)
+
+        case "Edit":
+            todo_to_edit = values['todos'][0]
+            new_todo = values['todo'] + "\n"
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+
+        case 'todos':
+            window['todo'].update(value=values['todos'][0])
+
         case sg.WIN_CLOSED:
             break
 
